3e6f59215f7eaab2424893f8610795d17f0ece9a
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-23 22:33:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  [{:name=>"Don Gately", :occupation=>"Live-in Staff"}, {:name=>"Joelle van Dyne", :occupation=>"Radio ...{:name=>"Kate Gompert", :occupation=>"None"}, {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+  [{:name=>"Don Gately", :occupation=>"Live-in Staff"}, {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}, {:name=>"Kate Gompert", :occupation=>"None"}, {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
@@ -26,5 +26,5 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
   aoh[row][key] = new_value
-  aoh_update
+  aoh
 end
